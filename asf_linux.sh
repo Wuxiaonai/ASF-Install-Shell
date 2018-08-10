3c2e1ede403356397a3b9851ce0cf1f4a69cd069
晨瑄 赵 <chenxuan.zhao@outlook.com>
2018-08-10 20:28:09 +0800

    1.修复脚本BUG 2.优化代码 3.解决部分国内VPS不能成功连接SteamCommunity

--- asf_linux.sh
+++ asf_linux.sh
@@ -20,6 +20,7 @@ Error="${red}[错误]${none}"
 Warning="${red}[警告]${none}"
 
 ArchiSteamFarmFile="/root/ASF/ArchiSteamFarm" 
+Account1File="/root/ASF/config/Account1.json" 
 
 checkRoot(){
 	[[ $EUID != 0 ]] && echo -e "${Error} 请使用ROOT账号或使用${green}sudo su${none}命令获取临时的ROOT权限" && exit 1
@@ -51,12 +52,10 @@ checkSystem(){
 installArchiSteamFarmInit(){
     #设置DNS
 	echo "正在设置DNS"
-	echo "
-	nameserver 1.1.1.1
-	nameserver 1.0.0.1
-	nameserver 223.5.5.5
-	nameserver 223.6.6.6
-	" >> /etc/resolv.conf
+	echo "nameserver 223.5.5.5
+nameserver 223.6.6.6
+nameserver 1.1.1.1
+nameserver 1.0.0.1" > /etc/resolv.conf
 	
 	#系统更新
 	echo "正在运行系统更新"
@@ -77,60 +76,80 @@ installArchiSteamFarmInit(){
 	#清理安装缓存
 	yum -y clean all
 	
-	echo "安装完成"
+	echo "安装完成！"
 }
 
 installArchiSteamFarm(){
 	#安装 ArchiSteamFarm
-	echo "正在下载ArchiSteamFarm"
-	wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.2.0.5/ASF-linux-x64.zip
-	echo "正在解压ArchiSteamFarm"
+	cd /root
+	echo "正在下载ArchiSteamFarm，请耐心等待。。。。。。"
+	wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.2.0.5/ASF-linux-x64.zip -O ASF-linux-x64.zip
+	echo "正在解压ArchiSteamFarm，请耐心等待。。。。。。"
 	unzip ASF-linux-x64.zip -d ASF/
 	rm -f /root/ASF-linux-x64.zip
 	
 	if [ ! -f "$ArchiSteamFarmFile" ]; then 
-		echo "ArchiSteamFarm安装失败，请重试"
+		echo "ArchiSteamFarm安装失败，请重试！"
 	else
-		echo "ArchiSteamFarm安装完成"
+		echo "ArchiSteamFarm安装完成！"
 	fi 
 }
 
 configArchiSteamFarm(){
-	cd /root/ASF
-	echo "输入你的Steam帐号";
-	read SteamID
-	echo "输入你的密码";
-	read Passord
-	cd ASF/config
-	touch Account1.json
-	echo -e "{
-	  "\"SteamLogin\"": "\"$SteamID\"",
-	  "\"SteamPassword\"": "\"$Passord\"",
-	  "\"Enabled\"": true,
-	  "\"IsBotAccount\"": false,
-	  "\"FarmOffline\"": true
-	}" > /root/ASF/config/Account1.json
-	echo "挂卡账户配置成功"
+	if [ ! -f "$ArchiSteamFarmFile" ]; then
+		echo "请先安装ArchiSteamFarm，再配置ArchiSteamFarm！"
+	else
+		cd /root/ASF
+		chmod 777 ArchiSteamFarm
+		echo "输入您的Steam帐号";
+		read SteamID
+		echo "输入您的密码";
+		read Passord
+		cd /root/ASF/config
+		touch Account1.json
+		echo -e "{
+		  "\"SteamLogin\"": "\"$SteamID\"",
+		  "\"SteamPassword\"": "\"$Passord\"",
+		  "\"Enabled\"": true,
+		  "\"IsBotAccount\"": false,
+		  "\"FarmOffline\"": true
+		}" > /root/ASF/config/Account1.json
+		
+		#加入Steamcommunity的IP
+		chmod 777 /etc/hosts
+		echo -e "184.26.216.215 store.steampowered.com
+		184.26.216.215 steamcommunity.com" > /etc/hosts
+		echo "挂卡账户配置成功！"
+	fi 
 } 
 
 runArchiSteamFarm(){
-	echo -e "23.50.18.229 steamcommunity.com" >> /etc/hosts 
-	echo && echo -e "
+	if [ ! -f "$ArchiSteamFarmFile" ]; then
+		echo "请先安装ArchiSteamFarm，再运行ArchiSteamFarm！"
+	else
+		if [ ! -f "$Account1File" ]; then
+			echo "请先配置ArchiSteamFarm挂卡账户，再运行ArchiSteamFarm！"
+		else 
+			echo && echo -e "
 	————————————————————————
 	开启ASF挂卡方法命令(先记下来之后再退出脚本)
 	按${red}[ Ctrl + C ]${none}退出脚本
 	${green}sudo screen -S ASF        ${none}
 	${green}cd /root/ASF/             ${none}
-	${green}chmod 777 ArchiSteamFarm  ${none}
 	${green}./ArchiSteamFarm          ${none}
-	
 	————————————————————————" && echo 
-	sleep 60s
+			sleep 60s
+		fi
+	fi
 }
 
 uninstallArchiSteamFarm(){
 	rm -rf /root/ASF
-	echo "ArchiSteamFarm卸载成功"
+	if [ ! -f "$ArchiSteamFarmFile" ]; then
+		echo "ArchiSteamFarm卸载成功！"
+	else
+		echo "ArchiSteamFarm卸载失败，请重试！"
+	fi
 }
 
 checkRoot
@@ -140,18 +159,18 @@ clear
 while :; do
 	echo && echo -e "  
 	---- 血小板が可爱い | ArchiSteamFarm一键云挂卡----
-	 ${red}[v${sh_ver}](Pre-release)${none}
+	${red}[v${sh_ver}](Pre-release)${none}
 	————————————————————————
-	
-	 ${green} 0.${none} 配置 ArchiSteamFarm 运行所需的环境
-	 ${green} 1.${none} 安装 ArchiSteamFarm
-	 ${green} 2.${none} 配置 ArchiSteamFarm
-	 ${green} 3.${none} 运行 ArchiSteamFarm
-	 ${green} 4.${none} 卸载 ArchiSteamFarm
 
-	 ————————————————————————
-	 按${red}[ Ctrl + C ]${none}退出脚本" && echo
-	stty erase '^H' && read -p " 请输入数字 [0-4]:" num
+	${green} 0.${none} 配置 ArchiSteamFarm 运行所需的环境
+	${green} 1.${none} 安装 ArchiSteamFarm
+	${green} 2.${none} 配置 ArchiSteamFarm 挂卡账户
+	${green} 3.${none} 运行 ArchiSteamFarm 方法
+	${green} 4.${none} 卸载 ArchiSteamFarm 
+
+	————————————————————————
+	按${red}[ Ctrl + C ]${none}退出脚本" && echo
+	stty erase '^H' && read -p "	请输入数字 [0-4]:" num
 	case "$num" in
 		0)
 		installArchiSteamFarmInit
